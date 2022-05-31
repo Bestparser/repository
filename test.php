d3d14fdc3b646c15d2a7bd3c564852ad1a7893d1
Kirill Sidorchenko <pas_first_cal@mail.ru>
2022-05-31 14:41:41 +0300

    a1
    a1

--- test.php
+++ test.php
@@ -1,22 +1,3 @@
-
-
-
-
-
-
-
-
-
 <?php
-<<<<<<< HEAD
-  echo 'R1 C6 M';
-  echo 'M2';
-  echo 'M3';
-  echo 'M4';
-=======
-  echo 'R1 C7 F';
-  echo 'F2';
-  echo 'F3';
-  echo 'F4';
->>>>>>> feature
+	echo 'a1'
 ?>
